ca486f9662d624c63c099751481fbc04a79527f7
ronixy <wxjgky@gmail.com>
2015-07-31 16:55:56 +0800

    Create resource.js

---
+++ resource.js
@@ -0,0 +1,22 @@
+var s_HelloWorld = "HelloWorld.jpg";
+var s_CloseNormal = "CloseNormal.png";
+var s_CloseSelected = "CloseSelected.png";
+var s_WatchOut = "watchout.png";
+var s_Plist = "watchout.plist";
+
+var g_resources = [
+    //image
+    s_HelloWorld,
+    s_CloseNormal,
+    s_CloseSelected,
+    s_WatchOut,
+    //plist
+    s_Plist
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
