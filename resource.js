52fb81bd1491c4b329db4fe1879319c038bc6d7b
ronixy <wxjgky@gmail.com>
2015-08-02 15:06:58 +0800

    Update resource.js

--- resource.js
+++ resource.js
@@ -3,6 +3,10 @@ var s_CloseNormal = "CloseNormal.png";
 var s_CloseSelected = "CloseSelected.png";
 var s_WatchOut = "watchout.png";
 var s_Plist = "watchout.plist";
+var m_button_touch = "sfx_button.mp3";
+var m_player_jump = "sfx_player_jump.mp3";
+var m_score = "sfx_score.mp3";
+var m_die = "sfx_player_die.mp3";
 
 var g_resources = [
     //image
@@ -11,12 +15,16 @@ var g_resources = [
     s_CloseSelected,
     s_WatchOut,
     //plist
-    s_Plist
+    s_Plist,
     //fnt
 
     //tmx
 
     //bgm
+    m_button_touch,
+    m_player_jump,
+    m_score,
+    m_die
 
     //effect
 ];
